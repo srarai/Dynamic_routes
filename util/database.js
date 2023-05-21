015ad5ebf639ec4cf9f0489aba8dc25cf0ee2293
srarai <112899238+srarai@users.noreply.github.com>
2023-05-21 15:43:34 +0530

    Add files via upload

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Newjourney@123'
+});
+
+module.exports = pool.promise();
